380150977d5e5a952fc69c1712674ca5f654c05f
Sarvesh Vyas <sarvesh.vyas.mec15@iitbhu.ac.in>
2020-05-09 10:30:58 +0530

    database added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config({ path: "./config/config.env" });
 const app = express();
-
+const db = require("./config/mongoose");
 
 
 
---
+++ config/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+// connect to local database
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+// display any error
+db.on("error", console.error.bind(console, "Error connecting to mongoDB"));
+
+
+// upon successful connection
+db.once('open', () => {
+    console.log("successfully connected to mongoDB")
+})
+
+
+module.exports = db;
